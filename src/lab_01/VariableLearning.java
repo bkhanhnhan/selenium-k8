60578a2a50b0c1a06d0384bd706f24c485fe2ef5
DESKTOP-LU4OV4P\ASUS <bkhanhnhan98@gmail.com>
2022-05-24 00:33:55 +0700

    test

---
+++ src/lab_01/VariableLearning.java
@@ -0,0 +1,9 @@
+package lab_01;
+
+public class VariableLearning {
+    public static void main(String[] args) {
+        short teo;
+        teo = 127;
+        System.out.println(Short.MAX_VALUE);
+    }
+}
---
+++ src/lession_02/OperatorLearning.java
@@ -0,0 +1,8 @@
+package lession_02;
+
+public class OperatorLearning {
+    public static void main(String[] args) {
+        int myNum1 = 1;
+        int myNum2 = 2;
+    }
+}
