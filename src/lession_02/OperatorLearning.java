54710961d065da6656a22f8da4b5085ae21e4dc4
Khanh <bkhanhnhan98@gmail.com>
2022-05-24 17:26:16 +0700

    commit message

--- src/lession_02/OperatorLearning.java
+++ src/lession_02/OperatorLearning.java
@@ -4,5 +4,10 @@ public class OperatorLearning {
     public static void main(String[] args) {
         int myNum1 = 1;
         int myNum2 = 2;
+
+        int total = myNum1 + myNum2;
+
+        System.out.println(5%2);
+        System.out.println(5/2);
     }
 }
