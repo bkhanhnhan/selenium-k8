437703df4923289462d11a06d0d7c5db1a52c099
Khanh <bkhanhnhan98@gmail.com>
2022-05-30 17:42:36 +0700

    lesson3

---
+++ src/lesson_03/ForLoop.java
@@ -0,0 +1,14 @@
+package lesson_03;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        final int MAX_NUM = 18;
+
+        //Infinitive loop (vong lap vo han)
+
+        for (byte number = 0; number < MAX_NUM;number++) {
+            System.out.println(number);
+        }
+    }
+}
