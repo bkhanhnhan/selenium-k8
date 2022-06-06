7f2b551b999a2eb8531e506a3290062bb22acdca
Khanh <bkhanhnhan98@gmail.com>
2022-06-06 12:07:41 +0700

    lesson 04

--- src/lab_03/MinToMax.java
+++ src/lab_03/MinToMax.java
@@ -5,7 +5,7 @@ public class MinToMax {
 
         int[] intArray = {1,10,4,5,6,7,5,2,0,11,23,0,14,30,1};
 
-        for (int index = 0; index < intArray.length; index++) {
+        for (int index = 0; index < intArray.length-1; index++) {
             for (int j = index+1; j < intArray.length; j++) {
                 if(intArray[index] > intArray[j]){
                     int a = intArray[index];
--- src/lesson_04/DoWhileLoop.java
+++ src/lesson_04/DoWhileLoop.java
@@ -1,2 +1,15 @@
-package lesson_04;public class DoWhileLoop {
+package lesson_04;
+
+import java.util.Scanner;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int guessingTime = 0;
+
+        do {
+            Scanner scanner = new Scanner()
+
+            guessingTime++;
+        } while (guessingTime < 3);
+    }
 }
