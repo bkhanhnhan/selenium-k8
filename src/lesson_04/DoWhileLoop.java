88244c2351190d3e4a1bec80331369db60ee3aa2
DESKTOP-LU4OV4P\ASUS <bkhanhnhan98@gmail.com>
2022-06-06 07:29:03 +0700

    lesson 04

--- src/lesson_04/DoWhileLoop.java
+++ src/lesson_04/DoWhileLoop.java
@@ -1,2 +1,25 @@
-package lesson_04;public class DoWhileLoop {
+package lesson_04;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int guessingTime = 0;
+        int randomNumber = new SecureRandom().nextInt(11);
+        System.out.println("Random number is: "+ randomNumber);
+
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Please input a number: ");
+            int userNumber = scanner.nextInt();
+            if (userNumber == randomNumber){
+                System.out.println("Hurray! ");
+                break;
+            }
+            guessingTime++;
+        } while (guessingTime < 3);
+        System.out.println("See you again!");
+    }
 }
+
---
+++ src/lesson_04/FunctionLearning.java
@@ -0,0 +1,14 @@
+package lesson_04;
+
+public class FunctionLearning {
+
+    public static void main(String[] args) {
+
+        sayHello("Teo", 12);
+        sayHello("Ti", 22 );
+        sayHello("Tun", 24);
+    }
+    public static void sayHello(String name, int age ){
+            System.out.println("Hello" + name + age);
+    }
+}
