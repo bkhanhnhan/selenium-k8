0ee16015f2a8bff15e0efa0829a5b0e26859e3b2
Khanh <bkhanhnhan98@gmail.com>
2022-06-09 17:12:47 +0700

    lesson 05

---
+++ src/lesson_05/ArrayListLearning.java
@@ -0,0 +1,17 @@
+package lesson_05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListLearning {
+
+    public static void main(String[] args) {
+        List<Integer> myArrList = new ArrayList<>();
+        myArrList(1);
+        myArrList(2);
+        myArrList(3);
+        myArrList(4);
+
+
+    }
+}
